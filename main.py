68814204388a99446ac5a18c323c701a23aa3a28
Ananya-20 <66774678+Ananya-20@users.noreply.github.com>
2020-11-12 01:22:18 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+form linux import *
+form docker import *
+form hadoop import *
+from aws import *
+
+while True:
+    os.system("clear")
+    os.system("tput setaf 1")
+    print("\t\t\t ----------------------")
+    print("\t\t\t Welcome To Our Program")
+    print("\t\t\t ----------------------")
+    os.system("tput setaf 7")
+    os.system("tput setaf 2")
+    print("""
+            Press 1: For Linux
+            Press 2: For Docker
+            Press 3: For Hadoop
+            Press 4: For Ansible
+            Press 5: For Amazon Web Services (AWS)
+            Press 6: For Exit
+            """)
+    print("\n")
+    os.system('tput setaf 3')
+    ch = int(input("Enter your choice:- "))
+    os.system('tput setaf 7')
+    if ch == 1:
+        LinuxMenu()
+    elif ch == 2:
+        DockerMenu()
+    elif ch == 3:
+        HadoopMenu()
+    elif ch == 4:
+        AnsibleMenu()
+    elif ch == 5:
+        AWS_Menu()
+    elif ch == 6:
+        os.system('clear')
+        print("\n")
+        os.system('tput setaf 1")
+        print("\t\t\t -------------------")
+        print("\t\t\t Thank You !! ")
+        print("\t\t\t -------------------")
+        os.system('tput setaf 7')
+        exit()
+
+
