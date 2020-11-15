f03333aedf81803e8c9d1c98fd45aa8fa55f0b5c
Akshansh <43261823+AkshanshSingh@users.noreply.github.com>
2020-11-15 21:10:00 +0530

    added some extra features
    Added features include:
    firefox, ping, installation and uninstallation of a package, addition and deletion of user and much more

--- linux.py
+++ linux.py
@@ -15,8 +15,15 @@ def Locally():
             Press 4: Create a directory
             Press 5: Create a file
             Press 6: Configure Web Server
-            Press 7: Exit
+            Press 7: Launch Firefox
+            Press 8: To add a new user
+            Press 9: To delete a new user
+            Press 10: To ping an IP address
+            Press 11: Install a package/software using yum
+            Press 12: Uninstall a package/software using yum
+            Press 13: Exit
             """)
+        ch = int(input("Enter your Choice:"))
         if ch == 1:
             print("Checking the IP...")
             os.system("ifconfig enp0s3")
@@ -25,23 +32,40 @@ def Locally():
         elif ch == 3:
             os.system("cal")
         elif ch == 4:
-            Dir_Nm = input("Enter the name of directory")
+            Dir_Nm = input("Enter the name of directory ")
             os.system("mkdir {}".format(Dir_NM))
             print("Directory Successfully Created...")
         elif ch == 5:
-            File_Nm = input("Enter the name of file")
+            File_Nm = input("Enter the name of file ")
             os.system("touch {}".format(File_Nm))
-            print("File Successfully Crested...")
+            print("File Successfully Created...")
         elif ch == 6:
             os.system("yum install httpd -y")
-            os.system("cp index.html /var/www/html/index.html")
+            os.system("cp index.html /var/www/html/index.html") #here index.html is a pre-created webpage in the same directory where this script is running
             os.system("systemctl start httpd")
             os.system("systemctl status httpd")
         elif ch == 7:
+            os.system("firefox")
+        elif ch == 8:
+            username = input("Enter a new username ")
+            os.system(f"useradd {user_name};id {user_name}")
+        elif ch == 9:
+            user_name=input("Enter the username To Delete : ")
+			os.system(f"userdel {user_name}")
+        elif ch == 10:
+            ip = input("Enter IP or Hostname you want to ping : ")
+			os.system("ping {}".format(ip))
+        elif ch == 11:
+            software = input("Enter package you want to install ")
+			os.system(f"yum install {software}")
+        elif ch == 12:
+            soft = input("\t\tEnter package you want to uninstall ")
+			os.system(f"yum remove {soft}")
+        elif ch == 13:
             break
         else:
             print("Enter a valid choice")
-            input()
+            print()
 
 
 def LinuxMenu():
