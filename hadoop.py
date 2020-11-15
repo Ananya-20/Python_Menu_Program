4edb75f71124ba9ad4a8c166d0ce3f1edc4805cb
Ananya-20 <66774678+Ananya-20@users.noreply.github.com>
2020-11-15 23:35:29 +0530

    hadoop.py

--- hadoop.py
+++ hadoop.py
@@ -1,2 +1,43 @@
-def hadoop():
-    pass
+import os
+
+print("Welcome To Hadoop...")
+
+def HadoopMenu():
+    print("""
+      Press 1: For Configuring Name(Master) Node
+      Press 2: For Configuring Data Node
+      """)
+    ch = int(input('Enter your choice'))
+    if ch == 1:
+        print("\nconfiguring Master Node\n")	       
+        ing=input("Enter Machine IP")
+        os.system("scp jdk-8u171-linux-x64.rpm {}:/".format(ing))	    
+        os.system("ssh {} rpm -i /jdk-8u171-linux-x64.rpm".format(ing))
+        os.system("scp hadoop-1.2.1-1.x86_64.rpm {}:/".format(ing))
+        os.system("ssh {} rpm -i /hadoop-1.2.1-1.x86_64.rpm --force".format(ing))
+        os.system("ssh {} jps".format(ing))
+        os.system("mkdir {} /nam".format(ing))
+        os.system("scp hdfs-site.xml {}:/etc/hadoop/hdfs-site.xml".format(ing))
+       	os.system("scp core-site.xml {}:/etc/hadoop/core-site.xml".format(ing))
+        os.system("ssh {} hadoop namenode -format".format(ing))
+        os.system("ssh {} hadoop-daemon.sh start namenode".format(ing))
+        os.system("ssh {} jps".format(ing))
+		
+    elif ch == 2:
+		    print("\nConfiguring Slave Node\n")
+       	ing=input("Enter Machine IP")
+       	os.system("scp jdk-8u171-linux-x64.rpm {}:/".format(ing))
+       	os.system("ssh {} rpm -i /jdk-8u171-linux-x64.rpm".format(ing))
+       	os.system("scp hadoop-1.2.1-1.x86_64.rpm {}:/".format(ing))
+       	os.system("ssh {} rpm -i /hadoop-1.2.1-1.x86_64.rpm --force".format(ing))
+       	os.system("ssh {} jps".format(ing))
+       	os.system("mkdir {} /data".format(ing))
+       	os.system("scp /home/Ganesh/hdfs-site.xml {}:/etc/hadoop/hdfs-site.xml".format(ing))
+       	os.system("scp /home/Ganesh/core-site.xml {}:/etc/hadoop/core-site.xml".format(ing))
+        os.system("ssh {} hadoop-daemon.sh start datanode".format(ing))        	
+        os.system("ssh {} jps".format(ing))
+     
+     elif ch == 3:
+        break           
+     else:
+            print("Enter valid choice")
